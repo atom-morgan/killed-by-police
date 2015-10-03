ba4e46c806b9d4d24b7396e7c7d1c1390326097d
Kyle Cline <kyledcline@gmail.com>
2015-10-03 13:09:20 -0500

    Update parser.rb

--- parser.rb
+++ parser.rb
@@ -7,44 +7,42 @@ puts "Starting to parse file: " + filename
 
 data = SmarterCSV.process(filename)
 
-myData = Array.new
-
-data.map do |item|
-  myHash = Hash.new
+my_data = data.map do |item|
+  my_hash = {}
   item.map do |key, value|
     if (key.to_s == 'date')
-      myHash[:reported_date] = value.gsub(/([(].*[)])/, '').strip
+      my_hash[:reported_date] = value.gsub(/([(].*[)])/, '').strip
     end
 
     if (key.to_s == 'state')
-      myHash[:geo_state] = value.to_s
+      my_hash[:geo_state] = value.to_s
     end
 
     if (key.to_s == 'gender/race')
       if value =~ /[MF][\/][a-zA-z]/
-        myHash[:gender] = value.gsub(/\/.$/, '').to_s
-        myHash[:race] = value.gsub(/[mfMF][\/]/, '').to_s
+        my_hash[:gender] = value.gsub(/\/.$/, '').to_s
+        my_hash[:race] = value.gsub(/[mfMF][\/]/, '').to_s
       else
         if value =~ /[MF]/
-          myHash[:gender] = value.to_s
-          myHash[:race] = nil
+          my_hash[:gender] = value.to_s
+          my_hash[:race] = nil
         end
         if value =~ /[WBILA]|(PI)/
-          myHash[:race] = value.to_s
-          myHash[:gender] = nil
+          my_hash[:race] = value.to_s
+          my_hash[:gender] = nil
         end
       end
     end
 
     if (key.to_s == 'name/age')
       if value =~ /,/
-        myHash[:name] = value.gsub(/,{1}.*/, '').strip.to_s
-        myHash[:age] = value.gsub(/.*,{1}/, '').strip.to_s
+        my_hash[:name] = value.gsub(/,{1}.*/, '').strip.to_s
+        my_hash[:age] = value.gsub(/.*,{1}/, '').strip.to_s
       else
         if value =~ /\d/
-          myHash[:age] = value.to_s
+          my_hash[:age] = value.to_s
         else
-          myHash[:name] = value.to_s
+          my_hash[:name] = value.to_s
         end
       end
     end
@@ -52,30 +50,29 @@ data.map do |item|
     if (key.to_s == 'unknown')
       item[key] = value.gsub(/\n/, '')
       if value.length > 1
-        myHash[:source_of_death] = value.tr("\n", ",")
+        my_hash[:source_of_death] = value.tr("\n", ",")
       else
-        myHash[:source_of_death] = value.to_s
+        my_hash[:source_of_death] = value.to_s
       end
     end
 
     if (key.to_s == 'kbp_link')
-      myHash[:kbp_link] = value.to_s
+      my_hash[:kbp_link] = value.to_s
     end
 
     if (key.to_s == 'news_link')
-      myHash[:news_link] = value.to_s
+      my_hash[:news_link] = value.to_s
     end
-
   end
-  myData.push(myHash)
+  my_hash
 end
 
-myData = myData.to_json
+my_data = my_data.to_json
 
-newFileName = /[kbp].*[^.csv]/.match(filename).to_s
+new_filename = /[kbp].*[^.csv]/.match(filename).to_s
 
-File.open("./json-data/" + newFileName + ".json", "w") do |f|
-  f.write(myData)
+File.open("./json-data/" + new_filename + ".json", "w") do |f|
+  f.write(my_data)
 end
 
-puts "JSON file created: json-data/" + newFileName + ".json"
+puts "JSON file created: json-data/" + new_filename + ".json"
